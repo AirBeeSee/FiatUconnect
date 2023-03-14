a72cfaf7e90d56132298697a8e61ed12b0250e72
AirBeeSee <57507903+AirBeeSee@users.noreply.github.com>
2023-03-14 09:09:35 +0100

    Update run.sh

--- FiatAddon/run.sh
+++ FiatAddon/run.sh
@@ -12,6 +12,9 @@ export FiatUconnect_FiatUser=$(bashio::config 'FiatUser')
 export FiatUconnect_FiatPw=$(bashio::config 'FiatPw')
 export FiatUconnect_FiatPin=$(bashio::config 'FiatPin')
 export FiatUconnect_Debug=$(bashio::config 'Debug')
+export FiatUconnect_RefreshInterval=$(bashio::config 'RefreshInterval')
+export FiatUconnect_AutoDeepRefresh=$(bashio::config 'AutoDeepRefresh')
+export FiatUconnect_StartDelaySeconds=$(bashio::config 'StartDelaySeconds')
 
 cd /build/
 ./FiatUconnect
